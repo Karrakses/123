ac36ec64be402f74de5a33abc1ebb0a82edd2e16
EgorovAV <123>
2023-02-15 16:47:59 +0300

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        book Son_Zeus = new book();
+        Son_Zeus.name = "Ñûם Çוגסא";
+        System.out.println(Son_Zeus.toString());
+    }
+}
---
+++ src/com/company/book.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class book {
+    String name;
+
+    @Override
+    public String toString(){
+        return "Название книги " + name;
+    }
+}
